530996f9c8170e50e8f06e0e663542586b75fcaf
katejoyreed <katejoyreed@gmail.com>
2021-03-25 12:37:43 -0500

    Completed logic for problem 2 to capitalize the first letter of every word input

--- index.js
+++ index.js
@@ -2,7 +2,17 @@
 reverseString();
 //problem 1: 
 function reverseString() {
-    let str = console.log("What should we reverse?")
-    let result = str.split("").reverse().join("");
-    console.log(result);
+    let str = document.getElementById("input").value;
+    document.getElementById("result").innerHTML = str.split("").reverse().join("");
+    
+}
+//problem 2:
+function capitalizeFirst(){
+    let sentence = document.getElementById("sentence").value;
+    const words = sentence.split(" ");
+    for (let i = 0; i < words.length; i++)
+   
+    document.getElementById("product").innerHTML = words.map((word) => {
+        return word[0].toUpperCase() + word.substring(1);
+    }).join(" ");
 }
