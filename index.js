3d3fbdf7d3942ad46f55f2a35e8765176efdaaca
katejoyreed <katejoyreed@gmail.com>
2021-03-25 11:22:44 -0500

    Added logic to Problem 1 to reverse a string

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 "use strict";
-
+reverseString();
 //problem 1: 
+function reverseString() {
+    let str = console.log("What should we reverse?")
+    let result = str.split("").reverse().join("");
+    console.log(result);
+}
